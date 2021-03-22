5d1fdbf7d27f8ecd015c22f5af4ccb9ea59ac2c5
Ivan Chien <qyc027@gmail.com>
2021-03-22 09:06:53 +0800

    Add zsh function

--- lisp/init-term.el
+++ lisp/init-term.el
@@ -14,7 +14,27 @@
             (display-line-numbers-mode 0)))
 
 ;; 设置终端全局键
-(global-set-key (kbd "C-c t") 'ansi-term)
+;;(global-set-key (kbd "C-c t") 'ansi-term)
+
+;; refs https://emacs.stackexchange.com/questions/48472/how-to-make-m-x-ansi-term-behave-like-m-x-shell-opening-in-new-window
+;; 下面的代码为了实现在新窗口中打开终端，这里选择的是 M-x ansi-term <RET> zsh <RET>
+
+;; 为 zsh 提供窗口模式
+(defun ns/my-term-p (name _action)
+  "Determine whether NAME names a `term-mode' buffer."
+  (with-current-buffer name
+    (derived-mode-p #'term-mode)))
+
+;; 直接打开 zsh
+(defun zsh ()
+  (interactive)
+  (let ((switch-to-buffer-obey-display-actions t))
+    (ansi-term "zsh")))
+
+(add-to-list 'display-buffer-alist '(ns/my-term-p () (inhibit-same-window . t)))
+
+;; 设置 zsh 快捷键
+(global-set-key (kbd "C-c t") 'zsh)
 
 
 (provide 'init-term)
