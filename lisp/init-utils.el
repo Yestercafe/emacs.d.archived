59b5fc0ecf165bc59f0cca5a5b4cef3df025eadd
Ivan Chien <qyc027@gmail.com>
2021-03-22 08:58:10 +0800

    Config which-key-mode and add undo-fu for redo feature

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -9,7 +9,19 @@
 (use-package which-key
   :ensure t
   :init
-  (which-key-mode))
+  (which-key-mode)
+  :config
+  ;; Allow C-h to trigger which-key before it is done automatically
+  (setq which-key-show-early-on-C-h t)
+  )
+
+;; undo-fu 为 Emacs 提供 redo 功能
+(use-package undo-fu
+  :ensure t
+  :bind
+  (("C-/" . undo-fu-only-undo)
+   ("C-S-/" . undo-fu-only-redo))
+  )
 
 
 (provide 'init-utils)
