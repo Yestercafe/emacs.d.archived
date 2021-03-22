b4e4ec13d44fd611e7315455eadb0356248baed9
Ivan Chien <qyc027@gmail.com>
2021-03-22 08:37:46 +0800

    Add which-key

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -1,8 +1,15 @@
 ;;; -*- lexical-binding: t -*-
 
+;; restart-emacs 重启 Emacs
 (use-package restart-emacs
   :ensure t
   )
+
+;; which-key 智能提示键位
+(use-package which-key
+  :ensure t
+  :init
+  (which-key-mode))
 
 
 (provide 'init-utils)
